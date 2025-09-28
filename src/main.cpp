b86ae0b4cb9b8394d0a84c1f76237675af37fad5
rkc137 <hlopov100@gmail.com>
2025-09-28 19:02:58 +0300

    now frames generates not from one past frame but from two

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "raywrap/Window.hpp"
 #include "raywrap/Draw.hpp"
+#include <raylib.h>
 
 #include <bitset>
 #include <array>
@@ -12,12 +13,13 @@ constexpr int w_width = 1500;
 constexpr int w_height = 1000;
 constexpr int X = w_width / rect_size;
 constexpr int Y = w_height / rect_size;
-const auto delay = std::chrono::milliseconds(100); 
+constexpr int past_size = 2;
 using Map = std::array<std::bitset<X + 2>, Y + 2>;
 
 void draw(const Map &map)
 {
     raywrap::draw([&](raywrap::DrawContext &ctx){
+        ctx.clear();
         for(int y = 0; y < Y; y++)
         for(int x = 0; x < X; x++)
             if(map[y + 1][x + 1])
@@ -25,20 +27,24 @@ void draw(const Map &map)
                     {rect_size * x, rect_size * y},
                     {rect_size,     rect_size}
                 );
-        ctx.clear();
     });
 }
 
-void sim_frame(Map &past, Map &future)
+void sim_frame(Map &past_1, Map &past_2, Map &future)
 {
     for(int y = 1; y < Y + 1; y++)
     for(int x = 1; x < X + 1; x++)
     {
-        int count = -past[y][x];
+        int count = -past_1[y][x] - past_2[y][x];
         for(int i : {-1, 0, 1})
         for(int j : {-1, 0, 1})
-            count += past[y + i][x + j];
-        future[y][x] = (past[y][x] && count == 2) || count == 3;
+            count += past_1[y + i][x + j] + past_2[y + i][x + j];
+
+        //for now past of cell itself didnt have role
+        if(4 > count || count > 8)
+            future[y][x] = false;
+        else if(4 <= count || count <= 8)
+            future[y][x] = true;
     }
 }
 
@@ -46,13 +52,14 @@ void setup(Map &map)
 {
     for(int y = 1; y < Y + 1; y++)
     for(int x = 1; x < X + 1; x++)
-        map[y][x] = !(rand() % 4);
+        map[y][x] = !(rand() % 8);
 }
 
 int main()
 {
-    std::array<Map, 2> maps;
+    std::array<Map, past_size + 1> maps;
     setup(maps[0]);
+    setup(maps[1]);
 
     const double target_fps = 10;
     const std::chrono::duration<double> frame_duration(1.0 / target_fps);
@@ -60,11 +67,22 @@ int main()
         {w_width, w_height}, "game of life raylib", 10
     );
     
-    for(bool turn = false; !raywrap::window::should_close(); turn = !turn)
+    for(unsigned int turn = 0; !raywrap::window::should_close(); turn++)
     {
+        std::array<Map*, past_size> pasts = {
+            &maps[(turn + 0) % maps.size()],
+            &maps[(turn + 1) % maps.size()]
+        };
+        auto &present = 
+            maps[(turn + 2) % maps.size()];
+        
+        if(IsKeyDown(KEY_SPACE))
+            for(auto past : pasts)
+                setup(*past);
+
         auto start_time = std::chrono::system_clock::now();
-        sim_frame(maps[turn], maps[!turn]);
-        draw(maps[turn]);
+        sim_frame(*pasts[0], *pasts[1], present);
+        draw(present);
         std::this_thread::sleep_for(
             frame_duration - (std::chrono::system_clock::now() - start_time)
         );
