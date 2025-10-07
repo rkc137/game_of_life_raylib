919f683376ddf045b8513a0a7196a34e6a8c9fde
rkc137 <hlopov100@gmail.com>
2025-10-07 15:19:45 +0300

    new rule for introverts

--- src/rules.hpp
+++ src/rules.hpp
@@ -24,9 +24,21 @@ const std::array<Extravert, 4> extraverts = {
     }
 };
 
-const std::array<Introvert, 1> introverts = {
+const std::array<Introvert, 2> introverts = {
+    //classical
     [](int count, int selfs_count) -> bool {
         return (count == 3 || (count == 2 && selfs_count));
+    },
+
+    //interesting with (past_size == 3) 
+    [](int count, int selfs_count) -> bool {
+        if(count < 2 * past_size || 
+           count > 3 * past_size)
+            return false;
+        if(count == 3 * past_size ||
+           count == 3 * past_size - 1)
+            return true;
+        return selfs_count == 1;
     }
 };
 
